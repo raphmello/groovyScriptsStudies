6eaa24c7d02c5af5ca6fe0206b946053fd3e9587
Raphael de Mello <raph.mello@gmail.com>
2025-01-31 12:55:38 -0300

    basic strctures

--- src/basics/ControlStructureLesson.groovy
+++ src/basics/ControlStructureLesson.groovy
@@ -1,4 +1,42 @@
 package basics
 
-class ControlStructureLesson {
+class ControlStructure {
+}
+
+static void main(String[] args) {
+    if (true)
+        println "value is true"
+
+    String name = null
+    if (name)
+        println "name is not null"
+    else
+        println "name is null"
+
+    def last = ""
+    if (last)
+        println "last is not empty"
+    else
+        println "last is empty"
+
+    def emptyList = []
+    if (emptyList)
+        println "emptyList is not empty"
+    else
+        println "emptyList is empty"
+
+    for (i in 0..<10) {
+        print i
+    }
+    println()
+
+    def list = [1, 2, 3, 4, 5]
+    for (item in list) {
+        print item
+    }
+    println()
+
+    def list2 = [1, 2, 3, 4, 5]
+    list2.each { print it}
+    println()
 }
